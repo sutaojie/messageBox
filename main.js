d6be4adfa46b5ab7818afba024c651464a465646
sutaojie <sutaojie@gmail.com>
2018-06-30 00:27:38 +0800

    MVC中 VC 部分

--- main.js
+++ main.js
@@ -1,50 +1,58 @@
-var APP_ID = 'H66LtI2x3YIJHYwiVI0nscEF-gzGzoHsz';
-var APP_KEY = '8GL22aDbFdkEf5XuuaxYOQuQ';
+! function() {
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
+    var view = document.querySelector('#messageBox')
+    var controller = {
+        view: null,
 
-var query = new AV.Query('Message');
-  query.find().then(function (message) {
-  	var messages = []
-   	message.map((item)=>{messages.push(item.attributes)})
-   	var messageList = document.querySelector('#messageList')
-   	messages.forEach((item)=>{
+        init: function(view) {
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('#messagePostForm')
+            this.initAV()
+            this.loadMessage()
+            this.bindEvent()
+        },
+        initAV: () => {
+            var APP_ID = 'H66LtI2x3YIJHYwiVI0nscEF-gzGzoHsz';
+            var APP_KEY = '8GL22aDbFdkEf5XuuaxYOQuQ';
+            AV.init({ appId: APP_ID, appKey: APP_KEY });
+        },
+        loadMessage: () => {
+            var query = new AV.Query('Message');
+            query.find().then(function(message) {
+                var messages = []
+                message.map((item) => { messages.push(item.attributes) })
+                messages.forEach((item) => {
+                    let li = document.createElement('li')
+                    li.innerText = `${item.name} : ${item.content}`
+                    this.messageList.appendChild(li)
+                })
+            });
+        },
+        bindEvent: function() {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.saveMessage()
+            })
+        },
+        saveMessage: () => {
+            let myForm = this.form
+            let content = document.querySelector('input[name=content]').value
+            let name = document.querySelector('input[name=name]').value
+            var Message = AV.Object.extend('Message');
+            var message = new Message();
+            message.save({
+                name: name,
+                content: content
+            }).then(function(object) {
+                let li = document.createElement('li')
+                li.innerText = `${object.attributes.name} : ${object.attributes.content}`
+                var messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+                document.querySelector('input[name=content]').value = ' '
 
-   		let li = document.createElement('li')
-   		li.innerText = `${item.name} : ${item.content}`
-   		messageList.appendChild(li)
-   	})
-  });
+            })
+        }
+    }
+    controller.init(view)
+}.call()
 
-let myForm = document.querySelector('#messagePostForm')
-myForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let content = document.querySelector('input[name=content]').value
-    let name = document.querySelector('input[name=name]').value
-    var Message = AV.Object.extend('Message');
-    var message = new Message();
-    message.save({
-    	name:name,
-        content: content
-    }).then(function(object) {
-      	let li = document.createElement('li')
-   		li.innerText = `${object.attributes.name} : ${object.attributes.content}`
-   		var messageList = document.querySelector('#messageList')
-   		messageList.appendChild(li)
-   		document.querySelector('input[name=content]').value = ' '
-   		
-    })
-})
-
-/*
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
-*/
